da49d83ebb0a64070becb681bdd2f740e6658b48
vgXhc <kliems@gmail.com>
2020-06-10 17:48:09 -0500

    Testing how to capture a map click

--- low_stress_map/app.R
+++ low_stress_map/app.R
@@ -1,99 +1,126 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(tidycensus)
+library(tmap)
+library(tidyverse)
+library(sf)
+library(leaflet)
 library(shiny)
 
-data(World, NLD_muni, NLD_prov, land, metro)
+options(tigris_use_cache = TRUE)
 
-current.mode <- tmap_mode("plot")
 
-# CASE 1: Facets defined by constant values
-tm_shape(World) +
-    tm_fill(c("forestgreen", "goldenrod")) +
-    tm_format("World", title=c("A green world", "A dry world"), bg.color="lightskyblue2", 
-              title.position=c("left", "bottom"))
+######################################################
+# get data
+######################################################
 
-# CASE 2: Facets defined by multiple variables
-tm_shape(World) +
-    tm_polygons(c("well_being", "life_exp"),
-                style=c("pretty", "fixed"), breaks=list(NULL, seq(45, 85, by = 5)),
-                palette=list("Oranges", "Purples"),
-                border.col = "black",
-                title=c("Well-Being Index", "Life Expectancy")) +
-    tm_format("World")
+##because I can't figure out how to use group_by for sf objects, I'm creating
+##separate objects for each of the ACS variables
 
-# \dontrun{
-#     tm_shape(NLD_muni) +
-#         tm_fill(c("pop_0_14", "pop_15_24", "pop_25_44", "pop_45_64", "pop_65plus"),
-#                 style="kmeans", 
-#                 palette=list("Oranges", "Greens", "Blues", "Purples", "Greys"),
-#                 title=c("Population 0 to 14", "Population 15 to 24", "Population 25 to 44",
-#                         "Population 45 to 64", "Population 65 and older")) +
-#         tm_shape(NLD_prov) +
-#         tm_borders() +
-#         tm_format("NLD", frame = TRUE, asp=0)
-# }
+###define function for getting the various variables
+
+get_acs5 <- function(x){
+    get_acs(geography = "tract",
+            variables = x,
+            state = "55",
+            county = "Dane",
+            geometry = TRUE)
+}
+
+##helper for identifying the relevant variables
+# variables <- load_variables(2018, "acs5/subject", cache = TRUE)
+
+# bike commute mode share
+bike_share <- get_acs5("S0801_C01_011")
+
+# percent households without vehicle available
+veh_avail <- get_acs5("S2504_C02_027")
+
+# median household income
+med_inc <- get_acs5("S1901_C02_012")
+
+
+
+# Aldermanic districts
+# shapefile https://opendata.arcgis.com/datasets/81039877861c40a1857b2e7634951e04_10.zip
+#download.file("https://opendata.arcgis.com/datasets/81039877861c40a1857b2e7634951e04_10.zip", "data/ald_dist.zip")
+#unzip("data/ald_dist.zip", exdir = "data")
+ald_dist <- read_sf(file.path(".","data", "4c9bfcdf-5c27-4997-9add-388fb5313aac202043-1-1k9c67z.t0l9.shp")) %>% 
+    st_make_valid() %>% #one polygon has some kind of error
+    mutate(district = as.factor(ALD_DIST))
+
+# low-stress bike network data
+
+traffic_stress <- read_sf(file.path(".", "data", "LTS", "Merge_FeatureToLine_Clean.shp")) 
+low_stress <- traffic_stress %>% 
+    filter(LTS_F <= 2) %>% 
+    mutate(LTS_F = as_factor(LTS_F))
+
+# overall road network data
+# this will allow filtering for roads that would easily be dieted
+# download.file("https://opendata.arcgis.com/datasets/55a0bff60c3b475893c6f483dd53cd40_1.csv")
+# unzip("data/Street_Centerlines_and_Pavement_Data-shp.zip", exdir = "data")
+roads <- read_sf("data/ab6ebe0a-d838-4e18-b4a2-7cc420e06232202044-1-1wweqis.8fsc.shp")
+
+multi_lane <- roads %>% 
+    filter(lanes > 2 | (lanes > 1 & oneway == 2))
+
+tm_shape(multi_lane) +
+    tm_lines()
+
+# RoadsCurrent <- st_read("data/Transportation.gdb.zip", layer = "RoadsCurrent")
 # 
-# # CASE 3: Facets defined by group-by variable(s)
-# # A group-by variable that divides the objects spatially
-# tm_shape(NLD_prov) +
-#     tm_polygons("gold2") +
-#     tm_facets(by="name")
+# RoadsCurrent <- RoadsCurrent %>% 
+#     filter(RdStatus == "Constructed" & civilMunicipality == "City of Madison")
 # 
-# \dontrun{
-#     tm_shape(NLD_muni) +
-#         tm_borders() +
-#         tm_facets(by="province") +
-#         tm_fill("population", style="kmeans", convert2density = TRUE) +
-#         tm_shape(NLD_prov) +
-#         tm_borders(lwd=4) +
-#         tm_facets(by="name")
-# }
-# 
-# # The objects are divided by a non-spatial variable (e.g. date/time)
-# if (require(dplyr) && require(tidyr)) {
-#     metro_long <- metro %>% 
-#         gather(year, population, -name, -name_long, -iso_a3, -geometry) %>% 
-#         mutate(year = as.integer(substr(year, 4, 7)))
-#     
-#     tm_shape(metro_long) +
-#         tm_bubbles("population") +
-#         tm_facets(by = "year")
-# }
-# \dontrun{
-#     tm_shape(land) +
-#         tm_raster("black") +
-#         tm_facets(by="cover_cls")
-# }
-# 
-# # Facets defined by two group-by variables
-# \dontrun{
-#     World$HPI3 <- cut(World$HPI, breaks = c(20, 35, 50, 65), 
-#                       labels = c("HPI low", "HPI medium", "HPI high"))
-#     World$GDP3 <- cut(World$gdp_cap_est, breaks = c(0, 5000, 20000, Inf), 
-#                       labels = c("GDP low", "GDP medium", "GDP high"))
-#     
-#     tm_shape(World) + 
-#         tm_fill("HPI3", palette="Dark2", colorNA="grey90", legend.show = FALSE) + 
-#         tm_facets(c("HPI3", "GDP3"), showNA=FALSE, free.coords = FALSE, drop.units = FALSE)
-#     
-#     metro_edited <- metro %>% 
-#         mutate(pop1950cat = cut(pop1950, breaks=c(0.5, 1, 1.5, 2, 3, 5, 10, 40)*1e6),
-#                pop2020cat = cut(pop2020, breaks=c(0.5, 1, 1.5, 2, 3, 5, 10, 40)*1e6))
-#     
-#     tm_shape(World) +
-#         tm_fill() +
-#         tm_shape(metro_edited) +
-#         tm_dots("red", size = .5) +
-#         tm_facets(c("pop1950cat", "pop2020cat"), free.coords = FALSE) +
-#         tm_layout(panel.label.rot = c(0, 90), panel.label.size = 2)
-# }
+# tm_shape(RoadsCurrent) +
+#     tm_lines(col = "LTS_Appr")
+######################################################
+# create map
+######################################################
 
-# restore current mode
-tmap_mode(current.mode)
+tmap_mode("view")
+
+t_map <- tm_shape(bike_share) +
+    tm_polygons("estimate", title = "% bike to work", alpha = 0.5, style = "jenks") +
+    tm_shape(veh_avail) +
+    tm_polygons("estimate", title = "% household w/o vehicle", alpha = 0.5, style = "jenks") +
+    tm_shape(med_inc) +
+    tm_polygons("estimate", title = "Median income", alpha = 0.5, style = "jenks") +
+    tm_shape(ald_dist) +
+    tm_polygons("district") +
+    tm_text("district")
+
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    leafletOutput("my_tmap")
+    
+)
+
+
+server <- function(input, output, session) {
+
+    output$my_tmap <- renderLeaflet({
+        tmap_leaflet(t_map)
+    })
+    
+    # When map is clicked, show a popup with coordinates
+    observe({
+        event <- input$my_tmap_shape_click
+        if (is.null(event))
+            return()
+        
+        isolate({
+            leafletProxy("my_tmap") %>% 
+                clearPopups() %>% 
+                addPopups(lng = event$lng,
+                          lat = event$lat, 
+                          popup = paste0(event$lat, event$lng))
+        })
+    })
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
